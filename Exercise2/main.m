0bccb8c22cd479ed763faa76b402482ccdf0d408
admin <dangtinhtong@gmail.com>
2017-11-20 12:52:02 +0700

    First commit

--- Exercise2/main.m
+++ Exercise2/main.m
@@ -2,16 +2,22 @@
 //  main.m
 //  Exercise2
 //
-//  Created by admin on 11/20/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/20/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
-
+// Write a program that convert 27° from degrees Fahrenheit (F) to degrees Celsius (C) using the following formula:
+//   C = (F-32)/1.8
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        float Fahrenheit = 27; // To declare variable Fahrenheit as float and assign its 27 value
+        float Celsius;
+        Celsius= (Fahrenheit - 32.0)/1.8;
+        NSLog(@"To convert 27 from degrees Fahrenheit (F) to degrees Celsius (C)");
+        NSLog(@" %f F = %f C", Fahrenheit, Celsius);
     }
     return 0;
 }
---
+++ Readme.md
@@ -0,0 +1,3 @@
+* Convert 27°from degrees Fahrenheit (F) to Celsius (C) using 
+* the following formula:
+* C = (F - 32)/1.8;
